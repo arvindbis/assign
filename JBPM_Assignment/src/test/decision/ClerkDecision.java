5ca43417a879b11361099fe10bdc746fa190e06d
Arvind Kumar <arvindbis@hotmail.com>
2016-04-19 14:13:03 +0530

    Update Process Defination

---
+++ JBPM_Assignment/src/test/decision/ClerkDecision.java
@@ -0,0 +1,13 @@
+package test.decision;
+
+import org.jbpm.graph.exe.ExecutionContext;
+import org.jbpm.graph.node.DecisionHandler;
+
+public class ClerkDecision implements DecisionHandler {
+
+	public String decide(ExecutionContext arg0) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
